2962e4ecf48eab6670cf4b4aa4910203c76b85b1
ryan <ryanlab03@gmail.com>
2021-01-04 14:40:27 +0100

    add creating response section from ad

---
+++ assets/js/createResponse.js
@@ -0,0 +1,18 @@
+global.showContent = function showContent() {
+
+    const responseSection = document.getElementById("responseSection");
+    const display = responseSection.style.display;
+
+    if ("none" == display) {
+        responseSection.style.display= "block";
+    } else {
+        responseSection.style.display= "none";
+    }
+
+}
+
+global.buildResponse = function showContent() {
+
+    
+
+}
